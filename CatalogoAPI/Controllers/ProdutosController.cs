a3d4e88729f53e6e9a4670ccb79b800c02a86a86
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-03-14 13:46:54 -0300

    Adding some methods to register products

---
+++ CatalogoAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,52 @@
+﻿using CatalogoAPI.Data;
+using CatalogoAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos == null)
+            {
+                return NotFound();
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}", Name = "ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(x => x.ProdutoId == id);
+            if (produto == null)
+            {
+                return NotFound("Não Encontrado");
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Produto produto)
+        {
+            if (produto == null)
+                BadRequest();
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+    }
+}
