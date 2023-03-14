3dfdd0ce39746586e0967ef16a5f211840204f7f
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-03-14 10:53:42 -0300

    Adding Migration

---
+++ CatalogoAPI/Migrations/20230313190746_PopularCategoria.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Categorias (Nome,ImagemUrl) values ('Bebidas','bebida.jpg')");
+            migrationBuilder.Sql("insert into Categorias (Nome,ImagemUrl) values ('Congelados','Biscoitos.jpg')");
+            migrationBuilder.Sql("insert into Categorias (Nome,ImagemUrl) values ('Biscoitos','Biscoitos.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Categorias");
+        }
+    }
+}
---
+++ CatalogoAPI/Migrations/20230313190814_PopularProdutos.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO PRODUTOS (Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " + "VALUES " + "('Ruffles','Pacote 75g',6.00,'Ruffles.jpg',50,now(),3)");
+            migrationBuilder.Sql("INSERT INTO PRODUTOS (Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " + "VALUES " + "('Coca Cola','Lata 350ml',4.00,'coca.jpg',50,now(),1)");
+            migrationBuilder.Sql("INSERT INTO PRODUTOS (Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " + "VALUES " + "('Lasanha Sadia Frango','Lasanha Congelada 300g',9.00,'lasanha.jpg',50,now(),2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Produtos");
+        }
+    }
+}
