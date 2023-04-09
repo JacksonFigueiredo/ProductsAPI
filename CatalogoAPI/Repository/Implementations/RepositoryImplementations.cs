d3e598b417be64051546ef9101dc2973e53a4dcc
jackson.figueiredo <jackson.figueiredo@valid.com>
2023-04-09 16:54:25 -0300

    Including Repository

---
+++ CatalogoAPI/Repository/Implementations/RepositoryImplementations.cs
@@ -0,0 +1,41 @@
+﻿using CatalogoAPI.Data;
+using CatalogoAPI.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoAPI.Repository.Implementations
+{
+    public class RepositoryImplementations<T> : IRepository<T> where T : class
+    {
+        protected AppDbContext _dbContext;
+
+        public RepositoryImplementations(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public void Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity); 
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+        public IQueryable<T> Get()
+        {
+           return _dbContext.Set<T>().AsNoTracking();
+        }
+
+        public T GetById(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+        {
+            return _dbContext.Set<T>().SingleOrDefault(expression);
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+        }
+    }
+}
