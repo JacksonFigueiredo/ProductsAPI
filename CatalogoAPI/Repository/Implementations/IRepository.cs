15458aea78bbf185e6f224cee1394e2e48ad42ee
jackson.figueiredo <jackson.figueiredo@valid.com>
2023-04-09 16:41:32 -0300

    Creating Interfaces

---
+++ CatalogoAPI/Repository/Implementations/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace CatalogoAPI.Repository.Implementations
+{
+    public interface IRepository<T> // An interface of anykind.
+    {
+        IQueryable<T> Get();
+        T GetById(Expression<Func<T, bool>> expression);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+
+    }
+}
